169b2edb7d772c11af015658a07b31c8a8ff007d
InEdited <ineditedofficial@gmail.com>
2019-11-01 18:20:57 +0200

    Initiating project

---
+++ BOTBOIS/Assets/CharacterController2D.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField, Tooltip("Max speed, in units per second, that the character moves.")]
+    float speed = 9;
+
+    [SerializeField, Tooltip("Acceleration while grounded.")]
+    float walkAcceleration = 75;
+
+    [SerializeField, Tooltip("Acceleration while in the air.")]
+    float airAcceleration = 30;
+
+    [SerializeField, Tooltip("Deceleration applied when character is grounded and not attempting to move.")]
+    float groundDeceleration = 70;
+
+    [SerializeField, Tooltip("Max height the character will jump regardless of gravity")]
+    float jumpHeight = 4;
+
+    public Rigidbody2D rigidBody;
+
+    private CircleCollider2D boxCollider;
+
+    private Vector2 velocity;
+
+    private float botSpeed;
+    /// <summary>
+    /// Set to true when the character intersects a collider beneath
+    /// them in the previous frame.
+    /// </summary>
+    private bool grounded;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<CircleCollider2D>();
+        botSpeed = speed * Random.Range(0.75f, 1.25f);
+    }
+
+    private void Update()
+    {
+        // Use GetAxisRaw to ensure our input is either 0, 1 or -1.
+        float moveInput = Input.GetAxis("Horizontal");
+
+
+
+
+        if (moveInput != 0)
+        {
+            velocity.x = moveInput * Time.deltaTime;
+            rigidBody.velocity = velocity * botSpeed;
+            
+
+        }
+        else
+        {
+            //velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+        }
+
+        
+        //transform.Translate(velocity * Time.deltaTime);
+
+        
+
+        // Retrieve all colliders we have intersected after velocity has been applied.
+
+    }
+}
---
+++ BOTBOIS/Assets/energy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class energy : MonoBehaviour
+{
+    int charge;
+    bool isInCharger;
+
+    CircleCollider2D collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(collider.OverlapCollider())
+        if (isInCharger)
+        {
+            Charge(); 
+
+        }
+        else
+        {
+
+        }
+    }
+    IEnumerator Charge()
+    {
+        charge = charge + 10;
+        yield return new WaitForSeconds(.1f);
+        
+    }
+}
